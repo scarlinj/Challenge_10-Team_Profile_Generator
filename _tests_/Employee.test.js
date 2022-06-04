dc4dd41d752ee6e69b6fc86ce8e1cf54cf13a64b
Stephen Carlin <scarlinj@gmail.com>
2022-06-03 21:15:25 -0700

    employee test works

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,31 @@
 const Employee = require('../lib/Employee.js');
 
-test('creates an employee object', () => {
-    const employee = new Employee('Employee1');
+test('Creates an employee object', () => {
+    const employeeCreate = new Employee();
   
-    expect(employee.name).toBe('Employee1');
-    expect(employee.value).toEqual(expect.any(Number));
+    expect(typeOf(employeeCreate)).toBe('object');
+  });
+
+  test("Test Name", () => {
+    const name = "Stephen";
+    const employeeCreate = new Employee(name);
+    expect(employeeCreate.name).toBe(name);
+  });  
+
+  test("Test ID", () => {
+    const id = 2;
+    const employeeCreate = new Employee("Stephen", id);
+    expect(employeeCreate.id).toBe(id);
+  });
+  
+  test("Test email", () => {
+    const email = "scarlinj@gmail.com";
+    const employeeCreate = new Employee("Stephen", 2, email);
+    expect(employeeCreate.email).toBe(email);
+  });
+
+  test("Testing role.", () => {
+    const returnValue = "Employee";
+    const employeeInstance = new Employee("Stephen", 2, "scarlinj@gmail.com");
+    expect(employeeInstance.getRole()).toBe(returnValue);
   });
